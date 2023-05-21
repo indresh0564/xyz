9ca11d7b72b31006f9cf497b42285476b57b1b6c
indresh0564 <indresh0564@gmail.com>
2023-05-21 10:40:20 +0530

    COMMITE

---
+++ studentobj.js
@@ -0,0 +1,14 @@
+const student={
+    name:'indresh',
+    age:23,
+    // greet:()=>{
+    //     console.log('hii i am'+this.name);
+    // }
+    // greet:function(){
+    //             console.log('hii i am'+this.name);
+    // }
+    greet(){
+                        console.log('hii i am'+this.name);
+    }
+}
+student.greet();
