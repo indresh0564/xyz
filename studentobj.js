639918a3884f066329c224c863dfcbc199a73184
indresh0564 <indresh0564@gmail.com>
2023-06-09 23:47:38 +0530

    xxx

--- studentobj.js
+++ studentobj.js
@@ -1,25 +1,58 @@
 const http = require('http');
+const fs = require('fs');
 const server=http.createServer((req,res)=>{
-// set response content  
-if(req.url=='/home') 
-{
-    res.write('<html><body><p>Welcome home</p></body></html>');
-    res.end();
-}
-else if(req.url=='/about') 
-{
-    res.write('<html><body><p>Welcome to About Us page.</p></body></html>');
-    res.end();
-}
-else if(req.url=='/node')
-{
-   res.write('<html><body><p>Welcome to my Node Js project.</p></body></html>');
-   res.end(); 
-}
-   console.log(req);
-//    res.end(); 
+    const url=req.url;
+    const method = req.method;
+    if(url==='/'){
+        res.write('<html>');
+        res.write('<head><title>enter massage</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>');
+        res.write('</html>');
+       return res.end(); 
+     if(url === '/message' && method === 'POST')
+     {
+      const body = [];
+       req.on('data',(chunk)=>{
+        console.log(chunk);
+        body.push(chunk);
+       });
+       req.on('end',()=>{
+        const parsedbody=Buffer.concat(body).toString();
+        console.log(parsedbody);
+       })
+       fs.writeFileSync('message.txt', 'DUMMY');
+       res.statusCode=302;
+       res.setHeader('Location','/');
+       return res.end();
+     }
+    // res.setHeader('Content-Type','text/html');
+    // res.write('<html>');
+    // res.write('<head><title>my first page</title></head>');
+    // res.write('<body><h1>enter the home page</h1></body>');
+    // res.write('</html>');
+    // res.end();
 });
-server.listen(5000);
+server.listen(3000);
+// set response content  
+// if(req.url=='/home') 
+// {
+//     res.write('<html><body><p>Welcome home</p></body></html>');
+//     res.end();
+// }
+// else if(req.url=='/about') 
+// {
+//     res.write('<html><body><p>Welcome to About Us page.</p></body></html>');
+//     res.end();
+// }
+// else if(req.url=='/node')
+// {
+//    res.write('<html><body><p>Welcome to my Node Js project.</p></body></html>');
+//    res.end(); 
+// }
+//    console.log(req);
+// //    res.end(); 
+// });
+// server.listen(5000);
 // const student={
 //     name:'indresh',
 //     age:23,
